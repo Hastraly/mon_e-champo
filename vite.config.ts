a609ee1b8f56e5ce229d3dfca21ec9458b8b4add
Hastraly <radix.kelian@gmail.com>
2025-10-04 14:46:01 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ Ajoute cette ligne pour corriger la page blanche sur Render
+  base: '/', // ✅ Ajoute cette ligne pour corriger la page blanche sur Render
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
