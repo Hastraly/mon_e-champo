830ca708f82b1149548185e9662c1a31db1aec80
Hastraly <radix.kelian@gmail.com>
2025-10-05 11:38:48 +0200

    Updated Credits.tsx

--- src/components/Credits.tsx
+++ src/components/Credits.tsx
@@ -36,7 +36,7 @@ export default function Credits() {
                   H
                 </div>
                 <div>
-                  <p className="font-bold text-slate-800 text-lg">Kélian</p>
+                  <p className="font-bold text-slate-800 text-lg">Kélian </p>
                   <p className="text-sm text-slate-600">Développeur Full-Stack</p>
                 </div>
               </div>
